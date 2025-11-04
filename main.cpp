7b3d056db0c11109e1a41163848e49e180d2d16f
Test User <test@example.com>
2025-11-04 05:13:51 +0000

    Simplified implementation with array-based storage

--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <string>
-#include "bptree.hpp"
 
 using std::string;
 using std::cin;
 using std::cout;
 
+const int MAX_USERS = 100000;
+const int MAX_TRAINS = 10000;
 const int MAX_STATIONS = 100;
-const int MAX_USERNAME = 25;
-const int MAX_PASSWORD = 35;
-const int MAX_NAME = 25;
-const int MAX_EMAIL = 35;
-const int MAX_TRAINID = 25;
-const int MAX_STATION_NAME = 40;
+const int MAX_ORDERS = 1000000;
 
 struct User {
-    char username[MAX_USERNAME];
-    char password[MAX_PASSWORD];
-    char name[MAX_NAME];
-    char mailAddr[MAX_EMAIL];
+    char username[25];
+    char password[35];
+    char name[25];
+    char mailAddr[35];
     int privilege;
     bool logged_in;
+    bool exists;
     
-    User() : privilege(0), logged_in(false) {
+    User() : privilege(0), logged_in(false), exists(false) {
         memset(username, 0, sizeof(username));
         memset(password, 0, sizeof(password));
         memset(name, 0, sizeof(name));
@@ -32,9 +30,9 @@ struct User {
 };
 
 struct Train {
-    char trainID[MAX_TRAINID];
+    char trainID[25];
     int stationNum;
-    char stations[MAX_STATIONS][MAX_STATION_NAME];
+    char stations[MAX_STATIONS][40];
     int seatNum;
     int prices[MAX_STATIONS];
     int startTime;
@@ -43,8 +41,9 @@ struct Train {
     int saleDate[2];
     char type;
     bool released;
+    bool exists;
     
-    Train() : stationNum(0), seatNum(0), startTime(0), type('G'), released(false) {
+    Train() : stationNum(0), seatNum(0), startTime(0), type('G'), released(false), exists(false) {
         memset(trainID, 0, sizeof(trainID));
         memset(prices, 0, sizeof(prices));
         memset(travelTimes, 0, sizeof(travelTimes));
@@ -53,38 +52,56 @@ struct Train {
     }
 };
 
-struct UserKey {
-    char username[MAX_USERNAME];
-    
-    UserKey() { memset(username, 0, sizeof(username)); }
-    UserKey(const char* s) { 
-        memset(username, 0, sizeof(username));
-        strcpy(username, s); 
-    }
-    
-    bool operator<(const UserKey& other) const {
-        return strcmp(username, other.username) < 0;
-    }
-};
-
-struct TrainKey {
-    char trainID[MAX_TRAINID];
-    
-    TrainKey() { memset(trainID, 0, sizeof(trainID)); }
-    TrainKey(const char* s) { 
-        memset(trainID, 0, sizeof(trainID));
-        strcpy(trainID, s); 
-    }
-    
-    bool operator<(const TrainKey& other) const {
-        return strcmp(trainID, other.trainID) < 0;
-    }
-};
-
-BPTree<UserKey, User> user_db("user.db");
-BPTree<TrainKey, Train> train_db("train.db");
-
+User users[MAX_USERS];
+Train trains[MAX_TRAINS];
 int user_count = 0;
+int train_count = 0;
+
+void load_data() {
+    std::ifstream uf("users.dat", std::ios::binary);
+    if (uf.is_open()) {
+        uf.read((char*)&user_count, sizeof(int));
+        uf.read((char*)users, sizeof(User) * user_count);
+        uf.close();
+    }
+    
+    std::ifstream tf("trains.dat", std::ios::binary);
+    if (tf.is_open()) {
+        tf.read((char*)&train_count, sizeof(int));
+        tf.read((char*)trains, sizeof(Train) * train_count);
+        tf.close();
+    }
+}
+
+void save_data() {
+    std::ofstream uf("users.dat", std::ios::binary);
+    uf.write((char*)&user_count, sizeof(int));
+    uf.write((char*)users, sizeof(User) * user_count);
+    uf.close();
+    
+    std::ofstream tf("trains.dat", std::ios::binary);
+    tf.write((char*)&train_count, sizeof(int));
+    tf.write((char*)trains, sizeof(Train) * train_count);
+    tf.close();
+}
+
+int find_user(const char* username) {
+    for (int i = 0; i < user_count; i++) {
+        if (users[i].exists && strcmp(users[i].username, username) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int find_train(const char* trainID) {
+    for (int i = 0; i < train_count; i++) {
+        if (trains[i].exists && strcmp(trains[i].trainID, trainID) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 void parse_time(const char* str, int& hour, int& minute) {
     sscanf(str, "%d:%d", &hour, &minute);
@@ -134,51 +151,49 @@ void cmd_add_user(const string& cmd) {
     string m = read_arg(cmd, "m");
     string g = read_arg(cmd, "g");
     
-    UserKey key(u.c_str());
-    User existing;
-    if (user_db.find(key, existing)) {
+    if (find_user(u.c_str()) != -1) {
         cout << "-1\n";
         return;
     }
     
-    if (user_count == 0) {
-        User new_user;
+    int active_users = 0;
+    for (int i = 0; i < user_count; i++) {
+        if (users[i].exists) active_users++;
+    }
+    
+    if (active_users == 0) {
+        User& new_user = users[user_count++];
         strcpy(new_user.username, u.c_str());
         strcpy(new_user.password, p.c_str());
         strcpy(new_user.name, n.c_str());
         strcpy(new_user.mailAddr, m.c_str());
         new_user.privilege = 10;
         new_user.logged_in = false;
-        
-        user_db.insert(key, new_user);
-        user_count++;
+        new_user.exists = true;
         cout << "0\n";
         return;
     }
     
-    UserKey cur_key(c.c_str());
-    User cur_user;
-    if (!user_db.find(cur_key, cur_user) || !cur_user.logged_in) {
+    int cur_idx = find_user(c.c_str());
+    if (cur_idx == -1 || !users[cur_idx].logged_in) {
         cout << "-1\n";
         return;
     }
     
     int privilege = std::stoi(g);
-    if (privilege >= cur_user.privilege) {
+    if (privilege >= users[cur_idx].privilege) {
         cout << "-1\n";
         return;
     }
     
-    User new_user;
+    User& new_user = users[user_count++];
     strcpy(new_user.username, u.c_str());
     strcpy(new_user.password, p.c_str());
     strcpy(new_user.name, n.c_str());
     strcpy(new_user.mailAddr, m.c_str());
     new_user.privilege = privilege;
     new_user.logged_in = false;
-    
-    user_db.insert(key, new_user);
-    user_count++;
+    new_user.exists = true;
     cout << "0\n";
 }
 
@@ -186,45 +201,41 @@ void cmd_login(const string& cmd) {
     string u = read_arg(cmd, "u");
     string p = read_arg(cmd, "p");
     
-    UserKey key(u.c_str());
-    User user;
-    if (!user_db.find(key, user)) {
+    int idx = find_user(u.c_str());
+    if (idx == -1) {
         cout << "-1\n";
         return;
     }
     
-    if (strcmp(user.password, p.c_str()) != 0) {
+    if (strcmp(users[idx].password, p.c_str()) != 0) {
         cout << "-1\n";
         return;
     }
     
-    if (user.logged_in) {
+    if (users[idx].logged_in) {
         cout << "-1\n";
         return;
     }
     
-    user.logged_in = true;
-    user_db.insert(key, user);
+    users[idx].logged_in = true;
     cout << "0\n";
 }
 
 void cmd_logout(const string& cmd) {
     string u = read_arg(cmd, "u");
     
-    UserKey key(u.c_str());
-    User user;
-    if (!user_db.find(key, user)) {
+    int idx = find_user(u.c_str());
+    if (idx == -1) {
         cout << "-1\n";
         return;
     }
     
-    if (!user.logged_in) {
+    if (!users[idx].logged_in) {
         cout << "-1\n";
         return;
     }
     
-    user.logged_in = false;
-    user_db.insert(key, user);
+    users[idx].logged_in = false;
     cout << "0\n";
 }
 
@@ -232,26 +243,24 @@ void cmd_query_profile(const string& cmd) {
     string c = read_arg(cmd, "c");
     string u = read_arg(cmd, "u");
     
-    UserKey cur_key(c.c_str());
-    User cur_user;
-    if (!user_db.find(cur_key, cur_user) || !cur_user.logged_in) {
+    int cur_idx = find_user(c.c_str());
+    if (cur_idx == -1 || !users[cur_idx].logged_in) {
         cout << "-1\n";
         return;
     }
     
-    UserKey key(u.c_str());
-    User user;
-    if (!user_db.find(key, user)) {
+    int idx = find_user(u.c_str());
+    if (idx == -1) {
         cout << "-1\n";
         return;
     }
     
-    if (cur_user.privilege <= user.privilege && strcmp(c.c_str(), u.c_str()) != 0) {
+    if (users[cur_idx].privilege <= users[idx].privilege && strcmp(c.c_str(), u.c_str()) != 0) {
         cout << "-1\n";
         return;
     }
     
-    cout << user.username << " " << user.name << " " << user.mailAddr << " " << user.privilege << "\n";
+    cout << users[idx].username << " " << users[idx].name << " " << users[idx].mailAddr << " " << users[idx].privilege << "\n";
 }
 
 void cmd_modify_profile(const string& cmd) {
@@ -262,60 +271,56 @@ void cmd_modify_profile(const string& cmd) {
     string m = read_arg(cmd, "m");
     string g = read_arg(cmd, "g");
     
-    UserKey cur_key(c.c_str());
-    User cur_user;
-    if (!user_db.find(cur_key, cur_user) || !cur_user.logged_in) {
+    int cur_idx = find_user(c.c_str());
+    if (cur_idx == -1 || !users[cur_idx].logged_in) {
         cout << "-1\n";
         return;
     }
     
-    UserKey key(u.c_str());
-    User user;
-    if (!user_db.find(key, user)) {
+    int idx = find_user(u.c_str());
+    if (idx == -1) {
         cout << "-1\n";
         return;
     }
     
-    if (cur_user.privilege <= user.privilege && strcmp(c.c_str(), u.c_str()) != 0) {
+    if (users[cur_idx].privilege <= users[idx].privilege && strcmp(c.c_str(), u.c_str()) != 0) {
         cout << "-1\n";
         return;
     }
     
-    if (!p.empty()) strcpy(user.password, p.c_str());
-    if (!n.empty()) strcpy(user.name, n.c_str());
-    if (!m.empty()) strcpy(user.mailAddr, m.c_str());
+    if (!p.empty()) strcpy(users[idx].password, p.c_str());
+    if (!n.empty()) strcpy(users[idx].name, n.c_str());
+    if (!m.empty()) strcpy(users[idx].mailAddr, m.c_str());
     if (!g.empty()) {
         int privilege = std::stoi(g);
-        if (privilege >= cur_user.privilege) {
+        if (privilege >= users[cur_idx].privilege) {
             cout << "-1\n";
             return;
         }
-        user.privilege = privilege;
+        users[idx].privilege = privilege;
     }
     
-    user_db.insert(key, user);
-    cout << user.username << " " << user.name << " " << user.mailAddr << " " << user.privilege << "\n";
+    cout << users[idx].username << " " << users[idx].name << " " << users[idx].mailAddr << " " << users[idx].privilege << "\n";
 }
 
 void cmd_add_train(const string& cmd) {
     string i = read_arg(cmd, "i");
-    
-    TrainKey key(i.c_str());
-    Train existing;
-    if (train_db.find(key, existing)) {
+
+    if (find_train(i.c_str()) != -1) {
         cout << "-1\n";
         return;
     }
-    
-    Train train;
+
+    Train& train = trains[train_count++];
     strcpy(train.trainID, i.c_str());
-    
+    train.exists = true;
+
     string n = read_arg(cmd, "n");
     train.stationNum = std::stoi(n);
-    
+
     string m = read_arg(cmd, "m");
     train.seatNum = std::stoi(m);
-    
+
     string s = read_arg(cmd, "s");
     int idx = 0;
     size_t pos = 0;
@@ -326,7 +331,7 @@ void cmd_add_train(const string& cmd) {
         strcpy(train.stations[idx++], station.c_str());
         pos = next + 1;
     }
-    
+
     string p = read_arg(cmd, "p");
     idx = 0;
     pos = 0;
@@ -336,12 +341,12 @@ void cmd_add_train(const string& cmd) {
         train.prices[idx++] = std::stoi(p.substr(pos, next - pos));
         pos = next + 1;
     }
-    
+
     string x = read_arg(cmd, "x");
     int hour, minute;
     parse_time(x.c_str(), hour, minute);
     train.startTime = time_to_minutes(hour, minute);
-    
+
     string t = read_arg(cmd, "t");
     idx = 0;
     pos = 0;
@@ -351,7 +356,7 @@ void cmd_add_train(const string& cmd) {
         train.travelTimes[idx++] = std::stoi(t.substr(pos, next - pos));
         pos = next + 1;
     }
-    
+
     string o = read_arg(cmd, "o");
     if (o != "_") {
         idx = 0;
@@ -363,7 +368,7 @@ void cmd_add_train(const string& cmd) {
             pos = next + 1;
         }
     }
-    
+
     string d = read_arg(cmd, "d");
     pos = d.find('|');
     int month, day;
@@ -371,33 +376,30 @@ void cmd_add_train(const string& cmd) {
     train.saleDate[0] = date_to_days(month, day);
     parse_date(d.substr(pos + 1).c_str(), month, day);
     train.saleDate[1] = date_to_days(month, day);
-    
+
     string y = read_arg(cmd, "y");
     train.type = y[0];
-    
+
     train.released = false;
-    
-    train_db.insert(key, train);
+
     cout << "0\n";
 }
 
 void cmd_release_train(const string& cmd) {
     string i = read_arg(cmd, "i");
-    
-    TrainKey key(i.c_str());
-    Train train;
-    if (!train_db.find(key, train)) {
+
+    int idx = find_train(i.c_str());
+    if (idx == -1) {
         cout << "-1\n";
         return;
     }
-    
-    if (train.released) {
+
+    if (trains[idx].released) {
         cout << "-1\n";
         return;
     }
-    
-    train.released = true;
-    train_db.insert(key, train);
+
+    trains[idx].released = true;
     cout << "0\n";
 }
 
@@ -405,12 +407,13 @@ void cmd_query_train(const string& cmd) {
     string i = read_arg(cmd, "i");
     string d = read_arg(cmd, "d");
 
-    TrainKey key(i.c_str());
-    Train train;
-    if (!train_db.find(key, train)) {
+    int idx = find_train(i.c_str());
+    if (idx == -1) {
         cout << "-1\n";
         return;
     }
+
+    Train& train = trains[idx];
 
     int month, day;
     parse_date(d.c_str(), month, day);
@@ -483,18 +486,18 @@ void cmd_query_train(const string& cmd) {
 void cmd_delete_train(const string& cmd) {
     string i = read_arg(cmd, "i");
 
-    TrainKey key(i.c_str());
-    Train train;
-    if (!train_db.find(key, train)) {
+    int idx = find_train(i.c_str());
+    if (idx == -1) {
         cout << "-1\n";
         return;
     }
 
-    if (train.released) {
+    if (trains[idx].released) {
         cout << "-1\n";
         return;
     }
 
+    trains[idx].exists = false;
     cout << "0\n";
 }
 
@@ -519,15 +522,22 @@ void cmd_refund_ticket(const string& cmd) {
 }
 
 void cmd_clean() {
-    user_db.clear();
-    train_db.clear();
     user_count = 0;
+    train_count = 0;
+    for (int i = 0; i < MAX_USERS; i++) {
+        users[i] = User();
+    }
+    for (int i = 0; i < MAX_TRAINS; i++) {
+        trains[i] = Train();
+    }
     cout << "0\n";
 }
 
 int main() {
     std::ios::sync_with_stdio(false);
     cin.tie(0);
+
+    load_data();
 
     string line;
     while (getline(cin, line)) {
@@ -537,6 +547,7 @@ int main() {
         string cmd_name = (cmd_end == string::npos) ? line : line.substr(0, cmd_end);
 
         if (cmd_name == "exit") {
+            save_data();
             cout << "bye\n";
             break;
         } else if (cmd_name == "add_user") {
